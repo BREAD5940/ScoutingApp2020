f43160472befb1f2cb019554a9b63e7e47b42f92
SophiaLed23 <sledoux23@dtechhs.org>
2020-01-16 21:12:56 -0800

    Create teleop.js
    basic forwards and back buttons

---
+++ teleop.js
@@ -0,0 +1,9 @@
+/*function to go back one page*/
+function toAutoPage() {
+  location.replace("auto.html")
+}
+
+/*function to go forward one page*/
+function toEndgamePage() {
+  location.replace("endgame.html")
+}
