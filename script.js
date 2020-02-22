096fc40a963eade061fdb3b4129c07b4a9411f3b
SophiaLed23 <sledoux23@dtechhs.org>
2020-02-21 18:11:13 -0800

    shield generator has a break
    in between the two words there is a break to make the screen width thinner

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function toAutoPage() {
 function flipInitiationLine() {
   var initiationLine = document.getElementById('start-slider');
   var sector = document.getElementById('sp-sector');
-  if (sector.innerHTML == ("Shield Generator")) {
+  if (sector.innerHTML == ("Shield <br> Generator")) {
   initiationLine.style.transform = 'rotate(270deg)';
   } else if (sector.innerHTML == ("Sector")){
     initiationLine.style.transform = 'rotate(90deg)';
@@ -23,7 +23,7 @@ function flipInitiationLine() {
 function flipSectorGenerator() {
   var sector = document.getElementById('sp-sector');
   if (sector.innerHTML == ("Sector")) {
-    sector.innerHTML = "Shield Generator";
+    sector.innerHTML = "Shield <br> Generator";
   } else {
     sector.innerHTML = "Sector";
   }
@@ -32,10 +32,10 @@ function flipSectorGenerator() {
 //changes the text from "shield generator" to "sector"
 function flipGeneratorSector() {
   var generator = document.getElementById('sp-generator');
-  if (generator.innerHTML == ("Shield Generator")) {
+  if (generator.innerHTML == ("Shield <br> Generator")) {
     generator.innerHTML = "Sector";
   } else {
-    generator.innerHTML = "Shield Generator";
+    generator.innerHTML = "Shield <br> Generator";
   }
 }
 
@@ -46,3 +46,28 @@ function flip() {
   flipInitiationLine();
   console.log("It worked!");
 }
+
+function countdown() {
+  var now = new Date().getTime();
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var twoMinutes = minutes * 2;
+  var thirtySeconds = seconds * 30;
+  var countdownTo = now + twoMinutes + thirtySeconds;
+  var distance = countdownTo - now;
+  document.getElementById("demo").innerHTML = distance;
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}
+
+//changes color of absent buttons when clicked
+/*function changeColorYes() {
+  document.getElementById("absent-yes").style.backgroundColor = "red";
+}
+
+function changeColorNo() {
+  document.getElementById("absent-no").style.backgroundColor = "blue";
+}*/
