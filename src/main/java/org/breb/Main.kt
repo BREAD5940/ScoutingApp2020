e9b0ea300a5b47f56772383f2260de1cda13dfa8
Matt <matthew.morley.ca@gmail.com>
2020-03-09 21:11:39 -0700

    redirect yes

--- src/main/java/org/breb/Main.kt
+++ src/main/java/org/breb/Main.kt
@@ -18,7 +18,7 @@ object Main {
             javalinConfig.addStaticFiles("web")
             javalinConfig.enableCorsForAllOrigins()
         }
-        app.post("/send") { handleData(it.body()) }
+        app.post("/send") { handleData(it.body()); it.redirect("index.html") }
         app.start(5800)
 
 //        handleData("img-size=%7B1368%2C672%7D&start-time=1583801422960&shots=%7B%28%2C%29%3BOUTER%3B1583801428757%7D%3B%7B%28%2C%29%3BMISS%3B1583801428757%7D%3B%7B%28%2C%29%3BMISS%3B1583801428757%7D%3B%7B%28%2C%29%3BMISS%3B1583801428757%7D%3B%7B%28370%2C168%29%3BINNER%3B1583801431982%7D%3B%7B%28370%2C168%29%3BINNER%3B1583801431982%7D%3B%7B%28370%2C168%29%3BINNER%3B1583801431982%7D%3B%7B%28370%2C168%29%3BINNER%3B1583801431982%7D%3B%7B%28370%2C168%29%3BINNER%3B1583801431982%7D%3B%7B%28370%2C168%29%3BINNER%3B1583801431982%7D%3B%7B%28370%2C168%29%3BMISS%3B1583801431982%7D%3B&climb-start=1583801443821&climb-end=1583801444498&cap1Time=&cap2Time=&cap3Time=&rotTime=&posTime=&Name=breb&Team%23=5940&Match%23=2&Alliance-station=Red2&startPostion=6&x=0&y=0&time=0&inner=0&outer=0&lower=0&miss=0&trench=0&pin=0&human=0&touch=0&rdz=0&protecc=0&tech=0&foul=0&climbSucc=on&buddy=on&total-rp=&total-points=&driveRank=5&playRank=3&defRank=1&defAvoidRank=3&notes=meh+ig&submit=Submit+Data")
@@ -138,7 +138,6 @@ object Main {
 //        }
 
 //        println("started at $startTime")
-//        ctx.redirect("index.html")
     }
 
     private fun submitToGoogleForms(entries: List<String>) {
